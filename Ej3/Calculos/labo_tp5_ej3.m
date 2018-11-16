5fd86789826c8d75fb14e69e58dba4e70bf71671
taomasgonzalez <togonzalez@itba.edu.ar>
2018-11-16 20:28:05 -0300

    tp terminado, falta chequear

--- Ej3/Calculos/labo_tp5_ej3.m
+++ Ej3/Calculos/labo_tp5_ej3.m
@@ -6,23 +6,24 @@ Am = Ap*m/2;
 t = 0:1/50/fp:3/fm;
 
 %Sm = sin(2*pi()*fm*t);
-Sp = sin(2*pi()*fp*t);
+Sp = Ap*sin(2*pi()*fp*t);
 
-Sm = Ap*(mod(t, 1/fm));
+Sm = (mod(t, 1/fm));
 
 SAM = Sp .* (1 + m*Sm);
 plot(t, SAM);
 
-x = [fp - fm, fp, fp + fm];
-y = 10*log10([Am^2/100/1e-3, Ap^2/100/1e-3, Am^2/100/1e-3]);
-figure;
-hold on;
-grid on;
-
-for i=1:3
-    %m=1: 18
-    %m=0.5: 24
-    quiver(x(i), -26, 0, y(i)+26, 0, 'Color', 'b', 'LineWidth', 3);
-end
-
-formataxes('', 'Frecuencia (Hz)', 'Potencia (dBm)');
+% 
+% x = [fp - fm, fp, fp + fm];
+% y = 10*log10([Am^2/100/1e-3, Ap^2/100/1e-3, Am^2/100/1e-3]);
+% figure;
+% hold on;
+% grid on;
+% 
+% for i=1:3
+%     %m=1: 18
+%     %m=0.5: 24
+%     quiver(x(i), -26, 0, y(i)+26, 0, 'Color', 'b', 'LineWidth', 3);
+% end
+% 
+% formataxes('', 'Frecuencia (Hz)', 'Potencia (dBm)');
